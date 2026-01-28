6a1b035d2bf4bf22c4e436ca7562f504762809da
Barbaro Javier <barbar0jav1er@callbackhell.run>
2026-01-28 17:22:24 +0400

    feat (ui): add checkout page

--- app/(app)/(auth)/order/_layout.tsx
+++ app/(app)/(auth)/order/_layout.tsx
@@ -1,8 +1,12 @@
 import { Stack } from "@/components/Stack";
 import { Colors } from "@/constants/theme";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 import React from "react";
+import { TouchableOpacity } from "react-native";
 
 const Layout = () => {
+  const router = useRouter();
   return (
     <Stack>
       <Stack.Screen
@@ -18,6 +22,26 @@ const Layout = () => {
           headerBackButtonDisplayMode: "minimal",
           title: "",
           contentStyle: { backgroundColor: Colors.background },
+        }}
+      />
+      <Stack.Screen
+        name="schedule"
+        options={{
+          presentation: "formSheet",
+          headerBackButtonDisplayMode: "minimal",
+          title: "Schedule Delivery",
+          sheetCornerRadius: 24,
+          sheetGrabberVisible: true,
+          sheetAllowedDetents: [0.5],
+          contentStyle: { backgroundColor: Colors.background },
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => router.dismiss()}
+              style={{ padding: 4, borderRadius: 20 }}
+            >
+              <Ionicons name="close-sharp" size={28} />
+            </TouchableOpacity>
+          ),
         }}
       />
     </Stack>
